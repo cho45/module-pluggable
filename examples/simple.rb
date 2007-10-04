9526c01e155c401f600e7c7463f65072f5a273b0
cho45 <cho45@lowreal.net>
2007-10-04 15:09:09 +0000

    lang/ruby/module-pluggable/README, lang/ruby/module-pluggable/examples/simple.rb:     めんどいので example 補充
    git-svn-id: http://svn.coderepos.org/share/lang/ruby/module-pluggable@366 d0d07461-0603-4401-acd4-de1884942a52

--- examples/simple.rb
+++ examples/simple.rb
@@ -3,9 +3,20 @@
 require "module/pluggable"
 
 class SimplePluggable
+	# pluggable [name=:plugins] [opts]
+	# pluggable make the name of instance method
+	# returning instance of Module::Pluggable::Plugin.
+	#
+	# All loaded plugins are in anonymous module,
+	# so you can't access the classes directly,
+	# and you can create some plugin-sets
+	# without confusing class variables etc.
 	pluggable
 
 	def initialize
+		# `init' method is not defined on Module::Pluggable::Plugin.
+		# undefined methods are delegated to `call' the plugins.
+		# In this case, `plugins.init(self)' is same as `plugins.call(:init, self)'.
 		plugins.init(self)
 	end
 
